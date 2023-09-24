fec45f2feaf0c0a94923254e98a433c9f27281f3
tunbudi06 <anjaytun2006.com>
2023-09-24 22:14:45 +0700

    upgrading to laravel 10

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -2,13 +2,13 @@
 <html lang="lang="{{ str_replace('_', '-', app()->getLocale()) }}"">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <link href="{{ asset('css/laravel.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     @yield('head')
 </head>
 <body>
     @include("layout.nav")
     @yield('content')
     @include("layout.footer")
-    <script src="{{ asset('js/laravel.js') }}" defer></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}" defer></script>
 </body>
 </html>
